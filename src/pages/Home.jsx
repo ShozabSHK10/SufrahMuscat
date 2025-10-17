a2f37cc9e9abe6123c8e9bd330429da334bedee8
ShozabSHK10 <shozabshk5@gmail.com>
2025-10-17 20:58:47 +0400

    added more spots and updated the css file accordingly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,51 @@ function App() {
   const spots = [
     {
       id: 1,
-      name: "Mohsin Burger",
-      spot_location: "Gubrah",
+      name: "Purple Illusion",
+      spot_location: "Void",
       imageUrl: "/assets/SpotImages/purple-illusion.jpg",
     },
     {
       id: 2,
-      name: "Camilia",
-      spot_location: "Ruwi",
+      name: "Christian Bale",
+      spot_location: "American Psycho",
       imageUrl: "/assets/SpotImages/bateman-8pool.jpg",
+    },
+    {
+      id: 3,
+      name: "FCB",
+      spot_location: "Spain",
+      imageUrl: "/assets/SpotImages/Barcelona-Emblem.jpg",
+    },
+    {
+      id: 4,
+      name: "Joao Cancelo",
+      spot_location: "Riyad",
+      imageUrl: "/assets/SpotImages/Cancelooo.jpg",
+    },
+    {
+      id: 5,
+      name: "Corvette CXR",
+      spot_location: "Salalah",
+      imageUrl: "/assets/SpotImages/ConceptCXR.png",
+    },
+    {
+      id: 6,
+      name: "Xavi Hernandez",
+      spot_location: "Barcelona",
+      imageUrl: "/assets/SpotImages/xavi.jpg",
+    },
+    {
+      id: 7,
+      name: "BMW M3 GTR",
+      spot_location: "New york",
+      imageUrl: "/assets/SpotImages/NFSMW.jpg",
+    },
+    {
+      id: 8,
+      name: "Bahjat Al Afkar",
+      spot_location: "Wadi Kabir",
+      imageUrl: "/assets/SpotImages/Logo.jpg",
     },
   ];
 
